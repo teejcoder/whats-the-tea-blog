cbc2aa1034b39123c991cef9f6567de792d81400
Teej <96551689+teejcoder@users.noreply.github.com>
2023-01-11 11:13:55 +1100

    Initial Commit
    Whats The Tea? React API BLOG

---
+++ README.md
@@ -0,0 +1,28 @@
+
+<h2 align="center">What's The Tea? React Blog</h2>
+
+<img src="https://media.giphy.com/media/xsgmnZFCp0NznqMB6l/giphy.gif" width="100%">
+
+(squint to view .gif, haha)
+
+<h2>What's The Tea? React Blog</h2>
+A Node MongoDB REST API using Express framework
+<br>
+
+
+## Tech Used
+Node.JS, MongoDB, Express, React, JavaScript, HTML, CSS, Axios
+
+## Run this code
+Open terminal in the code editor and run `yarn install` in the API folder to create Node_Modules dependencies. <br>
+Once installed run `yarn start`. <br>
+Split terminal and run `npm install` in the Client folder to create Node_Modules dependencies. <br>
+Once installed run `npm start`.<br>
+Browser should open in `http://localhost:3000` if not open browser and go to `http://localhost:3000`.
+
+## Optimizations
+Ideal optimizations:
+An Admin settings page displaying users and posts for quick mediation. 
+Social media APIs for socials integration. 
+Anchored category links for browsing of similar insterests.
+
---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats,setCats] = useState([]);
+
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:3000/images/"
+
+  useEffect(()=>{
+    const getCats = async ()=>
+    {
+      const res = await axios.get("/categories")
+      setCats(res.data)
+    }
+    getCats();
+  },[])
+
+
+  return (
+    <div className="sidebar">
+        <div className="sidebarItem">
+            <span className="sidebarTitle">ABOUT ME</span>
+            <img className="topImg" src={PF+user.profilePic} alt=""/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cum?</p>
+        </div>
+        <div className="sidebarItem">
+         <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+            <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+        </div>
+        <div className="sidebarTitle">FOLLOW US</div>
+        <div className="sidebarSocial">
+            <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+            <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+            <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+            <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+    </div>
+  )
+}
